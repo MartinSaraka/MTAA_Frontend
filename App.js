974bc023c8531071493160a133a26661c9d60374
sacredmasiv <sacredmasiv@gmail.com>
2022-04-09 10:57:37 +0200

    fix

--- App.js
+++ App.js
@@ -1,22 +1,20 @@
-
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState, useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import 'react-native-gesture-handler';
-import LandingPage from './src/screens/LandingPage';
-import LoginPage from './src/screens/LoginPage';
-<<<<<<< HEAD
-import RegisterPage from './src/screens/RegisterPage';
-=======
-import ContactPage from './src/screens/ContactPage';
-import CoachPage from './src/screens/CoachPage';
-import PricingPage from './src/screens/PricingPage';
-import AdminTrainingPage from './src/screens/AdminTrainingPage';
-import TrainingsPage from './src/screens/TrainingsPage';
-import ChangeTrainingPage from './src/screens/ChangeTrainingPage';
-import AddTrainingPage from './src/screens/AddTrainingPage';
->>>>>>> 619b1366ddce6e56ea32886f79c688a80d96a492
+import "react-native-gesture-handler";
+import LandingPage from "./src/screens/LandingPage";
+import LoginPage from "./src/screens/LoginPage";
+import RegisterPage from "./src/screens/RegisterPage";
+import ContactPage from "./src/screens/ContactPage";
+import CoachPage from "./src/screens/CoachPage";
+import PricingPage from "./src/screens/PricingPage";
+import AdminTrainingPage from "./src/screens/AdminTrainingPage";
+import TrainingsPage from "./src/screens/TrainingsPage";
+import ChangeTrainingPage from "./src/screens/ChangeTrainingPage";
+import AddTrainingPage from "./src/screens/AddTrainingPage";
+import DrawerNavigationPage from "./Screen/DrawerNavigationPage";
+
 const Stack = createStackNavigator();
 
 const App = () => {
@@ -26,41 +24,37 @@ const App = () => {
         screenOptions={{
           headerShown: false,
         }}
-        initialRouteName={"AdminTrainingPage"}
+        initialRouteName={"LandingPage"}
       >
         <Stack.Screen name="LandingPage" component={LandingPage} />
 
-
         <Stack.Screen name="LoginPage" component={LoginPage} />
         <Stack.Screen name="RegisterPage" component={RegisterPage} />
-
-          <Stack.Screen name="CoachPage" component={CoachPage} />
-          <Stack.Screen name="PricingPage" component={PricingPage} />
-          <Stack.Screen name="AdminTrainingPage" component={AdminTrainingPage} />
-          <Stack.Screen name="TrainingsPage" component={TrainingsPage} />
-          <Stack.Screen name="ContactPage" component={ContactPage} />
-          <Stack.Screen name="ChangeTrainingPage" component={ChangeTrainingPage} />
+        <Stack.Screen
+          name="DrawerNavigationPage"
+          component={DrawerNavigationPage}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="ContactPage" component={ContactPage} />
+        <Stack.Screen name="CoachPage" component={CoachPage} />
+        <Stack.Screen name="PricingPage" component={PricingPage} />
+        <Stack.Screen name="AdminTrainingPage" component={AdminTrainingPage} />
+        <Stack.Screen name="TrainingsPage" component={TrainingsPage} />
+        <Stack.Screen
+          name="ChangeTrainingPage"
+          component={ChangeTrainingPage}
+        />
         <Stack.Screen name="AddTrainingPage" component={AddTrainingPage} />
       </Stack.Navigator>
-
-
-
-
-
-
-
-
-
-
     </NavigationContainer>
-  )
-}
+  );
+};
 export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
